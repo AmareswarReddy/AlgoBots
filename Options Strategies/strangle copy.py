d5fb436aaff18da730db4f0cd2706110f364b3bb
PesalaVinayKumar <partyofwizard@gmail.com>
2021-08-29 19:57:03 +0530

    Update strangle copy.py

--- Options Strategies/strangle copy.py
+++ Options Strategies/strangle copy.py
@@ -5,6 +5,14 @@ from time import sleep
 from py5paisa import FivePaisaClient
 from py5paisa.strategy import *
 from cred import *
+main_str="BANKNIFTY 02 SEP 2021 "
+main_str_format = "BANKNIFTY 02 Sep 2021 "
+main_str_pe = main_str+"PE "
+main_str_ce = main_str+"CE "
+main_str_format_pe=main_str_format+"PE "
+main_str_format_ce=main_str_format+"CE "
+expiry = "20210902"
+expiry_format= expiry[:4]+'-'+expiry[4:6]+'-'+expiry[6:]
 day=int(input('enter the no. of days ellapsed since strategy implimentation :'))
 if day!=0:
     Current_CE_strikeprice=input('enter the holding ce strike price (eg:36100) :')
@@ -24,8 +32,7 @@ Client.login()
 #NOTE : Symbol has to be in the same format as specified in the example below.
 #banknifty scripcode=999920005
 #N	C	999920005	BANKNIFTY 	EQ	1980-01-01 00:00:00	EQ	0	Z  BANKNIFTY                                         
-req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"},
-            {"Exch":"N","ExchType":"D","Symbol":"NIFTY 02 SEP 2021 PE 15500.00","Expiry":"20210902","StrikePrice":"15500","OptionType":"PE"}]          
+req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]          
 a=Client.fetch_market_feed(req_list_)
 
 '''
@@ -44,14 +51,14 @@ let the nifty bank is trading at 36000
 """
 #x = input('Bank Nifty Value on the day of taking the trades')
 x=a['Data'][0]['LastRate']
-req_list_PE=[{"Exch":"N","ExchType":"D","Symbol":"BANKNIFTY 02 SEP 2021 PE "+str(int(x/100)*100)+".00","Expiry":"20210902","StrikePrice":str(int(x/100)*100),"OptionType":"PE"}]
-req_list_CE=[{"Exch":"N","ExchType":"D","Symbol":"BANKNIFTY 02 SEP 2021 CE "+str(int(x/100)*100)+".00","Expiry":"20210902","StrikePrice":str(int(x/100)*100),"OptionType":"CE"}]
+req_list_PE=[{"Exch":"N","ExchType":"D","Symbol":main_str_pe+str(int(x/100)*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100),"OptionType":"PE"}]
+req_list_CE=[{"Exch":"N","ExchType":"D","Symbol":main_str_ce+str(int(x/100)*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100),"OptionType":"CE"}]
 req_list_PE_strikeprice=[int(x/100)*100]
 req_list_CE_strikeprice=[int(x/100)*100]
 for i in range(1,25):
-    req_list_PE=req_list_PE+[{"Exch":"N","ExchType":"D","Symbol":"BANKNIFTY 02 SEP 2021 PE "+str(int(x/100)*100-i*100)+".00","Expiry":"20210902","StrikePrice":str(int(x/100)*100-i*100),"OptionType":"PE"}] 
+    req_list_PE=req_list_PE+[{"Exch":"N","ExchType":"D","Symbol": main_str_pe+str(int(x/100)*100-i*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100-i*100),"OptionType":"PE"}] 
     req_list_PE_strikeprice=req_list_PE_strikeprice+[int(x/100)*100-i*100]
-    req_list_CE=req_list_CE+[{"Exch":"N","ExchType":"D","Symbol":"BANKNIFTY 02 SEP 2021 CE "+str(int(x/100)*100+i*100)+".00","Expiry":"20210902","StrikePrice":str(int(x/100)*100+i*100),"OptionType":"CE"}] 
+    req_list_CE=req_list_CE+[{"Exch":"N","ExchType":"D","Symbol":main_str_ce+str(int(x/100)*100+i*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100+i*100),"OptionType":"CE"}] 
     req_list_CE_strikeprice=req_list_CE_strikeprice+[int(x/100)*100+i*100]
 live_PE=Client.fetch_market_feed(req_list_PE)
 live_PE_lastrate=[]
@@ -74,7 +81,7 @@ strategy=strategies(user="@gmail.com", passw="PASSWORD", dob="YYYYMMDD",cred=cre
 #short_strangle(<symbol>,<List of sell strike price>,<qty>,<expiry>,<Order Type>)
 #strategy.short_strangle('banknifty',[str(PE_lower),str(CE_upper)],'25','20210902','D')
 #iron_condor(<symbol>,<List of buy strike prices>,<List of sell strike price>,<qty>,<expiry>,<Order Type>)
-strategy.iron_condor("banknifty",[str(CE_hedge),str(PE_hedge)],[str(PE_lower),str(CE_upper)],'25','20210902','D')
+strategy.iron_condor("banknifty",[str(CE_hedge),str(PE_hedge)],[str(PE_lower),str(CE_upper)],'25',expiry,'D')
 positions= Client.positions()
 
 CE_req=req_list_CE[CE_index_strikeprice]
@@ -86,7 +93,7 @@ if day!=0:
             CE_req=req_list_CE
             break
     for j in range(0,len(req_list_PE)):
-        if req_list_PE[i]==Current_PE_strikeprice:
+        if req_list_PE[j]==Current_PE_strikeprice:
             PE_req=req_list_PE
             break
 
@@ -105,10 +112,10 @@ while True:
                 req=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]          
                 a=Client.fetch_market_feed(req)
                 x=a['Data'][0]['LastRate']
-                req_list_PE=[{"Exch":"N","ExchType":"D","Symbol":"BANKNIFTY 02 SEP 2021 PE "+str(int(x/100)*100)+".00","Expiry":"20210902","StrikePrice":str(int(x/100)*100),"OptionType":"PE"}]
+                req_list_PE=[{"Exch":"N","ExchType":"D","Symbol":main_str_pe+str(int(x/100)*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100),"OptionType":"PE"}]
                 req_list_PE_strikeprice=[int(x/100)*100]
                 for i in range(1,25):
-                    req_list_PE=req_list_PE+[{"Exch":"N","ExchType":"D","Symbol":"BANKNIFTY 02 SEP 2021 PE "+str(int(x/100)*100-i*100)+".00","Expiry":"20210902","StrikePrice":str(int(x/100)*100-i*100),"OptionType":"PE"}] 
+                    req_list_PE=req_list_PE+[{"Exch":"N","ExchType":"D","Symbol":main_str_pe+str(int(x/100)*100-i*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100-i*100),"OptionType":"PE"}] 
                     req_list_PE_strikeprice=req_list_PE_strikeprice+[int(x/100)*100-i*100]
                 live_PE=Client.fetch_market_feed(req_list_PE)
                 live_PE_lastrate=[]
@@ -120,7 +127,7 @@ while True:
                 Client.place_order(test_order)
                 
                 #sell pe which is 80 to 95% of ce
-                scripcode_=str(int(script[script['Expiry']=='2021-09-02 14:30:00'][script['StrikeRate']==live_PE_lastrate[PE_index_strikeprice]][script['CpType']=='PE']['Scripcode']))
+                scripcode_=str(int(script[script['Expiry']==expiry_format+' 14:30:00'][script['StrikeRate']==live_PE_lastrate[PE_index_strikeprice]][script['CpType']=='PE']['Scripcode']))
                 test_order2=Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=scripcode_, quantity=25,price=0,is_intraday=False,atmarket=True)
                 Client.place_order(test_order2)
                 PE_req = req_list_PE[PE_index_strikeprice]
@@ -135,10 +142,10 @@ while True:
                 req=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]          
                 a=Client.fetch_market_feed(req)
                 x=a['Data'][0]['LastRate']
-                req_list_CE=[{"Exch":"N","ExchType":"D","Symbol":"BANKNIFTY 02 SEP 2021 CE "+str(int(x/100)*100)+".00","Expiry":"20210902","StrikePrice":str(int(x/100)*100),"OptionType":"CE"}]
+                req_list_CE=[{"Exch":"N","ExchType":"D","Symbol":main_str_ce+str(int(x/100)*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100),"OptionType":"CE"}]
                 req_list_PE_strikeprice=[int(x/100)*100]
                 for i in range(1,25):
-                    req_list_CE=req_list_CE+[{"Exch":"N","ExchType":"D","Symbol":"BANKNIFTY 02 SEP 2021 CE "+str(int(x/100)*100+i*100)+".00","Expiry":"20210902","StrikePrice":str(int(x/100)*100-i*100),"OptionType":"CE"}] 
+                    req_list_CE=req_list_CE+[{"Exch":"N","ExchType":"D","Symbol":main_str_ce+str(int(x/100)*100+i*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100-i*100),"OptionType":"CE"}] 
                     req_list_CE_strikeprice=req_list_CE_strikeprice+[int(x/100)*100+i*100]
                 live_CE=Client.fetch_market_feed(req_list_CE)
                 live_CE_lastrate=[]
@@ -150,7 +157,7 @@ while True:
                 Client.place_order(test_order)
                 
                 #sell pe which is 80 to 95% of ce
-                scripcode_=str(int(script[script['Expiry']=='2021-09-02 14:30:00'][script['StrikeRate']==live_CE_lastrate[CE_index_strikeprice]][script['CpType']=='CE']['Scripcode']))
+                scripcode_=str(int(script[script['Expiry']==expiry_format+' 14:30:00'][script['StrikeRate']==live_CE_lastrate[CE_index_strikeprice]][script['CpType']=='CE']['Scripcode']))
                 test_order2=Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=scripcode_, quantity=25,price=0,is_intraday=False,atmarket=True)
                 Client.place_order(test_order2)
                 CE_req = req_list_CE[CE_index_strikeprice]
@@ -162,13 +169,13 @@ while True:
             Total_value_old=Total_value_new
         if Total_value_new>Stop_loss:
             #square off all positions
-            test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=str(int(script[script['FullName']=='BANKNIFTY 02 Sep 2021 PE '+CE_req['StrikePrice']+'.00'])), quantity=25,price=0,is_intraday=False,atmarket=True)
+            test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=str(int(script[script['FullName']==main_str_format_pe+CE_req['StrikePrice']+'.00'])), quantity=25,price=0,is_intraday=False,atmarket=True)
             Client.place_order(test_order)
-            test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=str(int(script[script['FullName']=='BANKNIFTY 02 Sep 2021 CE '+CE_req['StrikePrice']+'.00'])), quantity=25,price=0,is_intraday=False,atmarket=True)
+            test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=str(int(script[script['FullName']==main_str_format_ce+CE_req['StrikePrice']+'.00'])), quantity=25,price=0,is_intraday=False,atmarket=True)
             Client.place_order(test_order)
-            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=str(int(script[script['FullName']=='BANKNIFTY 02 Sep 2021 CE '+str(CE_hedge)+'.00'])), quantity=25,price=0,is_intraday=False,atmarket=True)
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=str(int(script[script['FullName']==main_str_format_ce+str(CE_hedge)+'.00'])), quantity=25,price=0,is_intraday=False,atmarket=True)
             Client.place_order(test_order)
-            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=str(int(script[script['FullName']=='BANKNIFTY 02 Sep 2021 PE '+str(PE_hedge)+'.00'])), quantity=25,price=0,is_intraday=False,atmarket=True)
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=str(int(script[script['FullName']==main_str_format_pe+str(PE_hedge)+'.00'])), quantity=25,price=0,is_intraday=False,atmarket=True)
             Client.place_order(test_order)
 
 
