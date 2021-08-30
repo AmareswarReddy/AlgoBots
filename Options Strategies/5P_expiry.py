0e809669f90d9da4e43be2a59456449bb03e1abd
PesalaVinayKumar <partyofwizard@gmail.com>
2021-08-30 05:45:50 +0530

    full code till adjustment

--- Options Strategies/5P_expiry.py
+++ Options Strategies/5P_expiry.py
@@ -7,48 +7,105 @@ from py5paisa.strategy import *
 from cred import *
 main_str="BANKNIFTY 02 SEP 2021 "
 main_str_format = "BANKNIFTY 02 Sep 2021 "
-main_str_pe = main_str+"PE "
-main_str_ce = main_str+"CE "
-main_str_format_pe=main_str_format+"PE "
-main_str_format_ce=main_str_format+"CE "
 expiry = "20210902"
 expiry_format= expiry[:4]+'-'+expiry[4:6]+'-'+expiry[6:]
 script=pd.read_csv('scripmaster-csv-format.csv')
 cred={
-            "APP_NAME":,
-            "APP_SOURCE":,
-            "USER_ID":,
-            "PASSWORD":,
-            "USER_KEY":,
-            "ENCRYPTION_KEY":
-        }
-Client = FivePaisaClient(email='@gmail.com', passwd='password',dob='yyyymmdd', cred=cred)
+    "APP_NAME":,
+    "APP_SOURCE":,
+    "USER_ID":,
+    "PASSWORD":,
+    "USER_KEY":,
+    "ENCRYPTION_KEY":
+    }
+Client = FivePaisaClient(email='vinaykumar7295@gmail.com', passwd='godofwarvinay1@A',dob='19700701', cred=cred)
 Client.login()
 #%%
-req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]          
+req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]
 a=Client.fetch_market_feed(req_list_)
 x=a['Data'][0]['LastRate']
+floor_strikeprice=str(int(np.floor(x/100)*100))
+ceil_strikeprice=str(int(np.ceil(x/100)*100))
+req_list_PE=[{"Exch":"N","ExchType":"D","Symbol":main_str+'PE '+floor_strikeprice+".00","Expiry":expiry,"StrikePrice":floor_strikeprice,"OptionType":"PE"},
+                      {"Exch":"N","ExchType":"D","Symbol":main_str+'PE '+ceil_strikeprice+".00","Expiry":expiry,"StrikePrice":ceil_strikeprice,"OptionType":"PE"}]
+req_list_CE=[{"Exch":"N","ExchType":"D","Symbol":main_str+'CE '+floor_strikeprice+".00","Expiry":expiry,"StrikePrice":floor_strikeprice,"OptionType":"CE"},
+                       {"Exch":"N","ExchType":"D","Symbol":main_str+'CE '+ceil_strikeprice+".00","Expiry":expiry,"StrikePrice":ceil_strikeprice,"OptionType":"CE"}]
+live_PE = Client.fetch_market_feed(req_list_PE)
+live_CE = Client.fetch_market_feed(req_list_CE)
+live_PE_lastrate = [live_PE['Data'][0]['LastRate']]+[live_PE['Data'][1]['LastRate']]
+live_CE_lastrate = [live_CE['Data'][0]['LastRate']]+[live_CE['Data'][1]['LastRate']]
+strategy_sell_value=np.max(live_PE_lastrate+live_CE_lastrate)
+temp=np.argmax(live_PE_lastrate+live_CE_lastrate)
+if temp<=1:
+    strategy_on='PE '
+    hole=0
+else :
+    strategy_on='CE '
+    hole=1
+if temp%2==0:
+    strategy_strikeprice=int(floor_strikeprice)
+else :
+    strategy_strikeprice=int(ceil_strikeprice)
+
+temp2=np.ceil(strategy_sell_value/100)*100
+strategy_lower_strikeprice=str(int(strategy_strikeprice-temp2))
+strategy_upper_strikeprice=str(int(strategy_strikeprice+temp2))
+strategy_on_strikeprice=str(int(strategy_strikeprice))
+main_str_name = main_str+strategy_on
+main_str_name_format=main_str_format+strategy_on
 
 #%%
-req_list_PE=[{"Exch":"N","ExchType":"D","Symbol":main_str_pe+str(int(x/100)*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100),"OptionType":"PE"}]
-req_list_CE=[{"Exch":"N","ExchType":"D","Symbol":main_str_ce+str(int(x/100)*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100),"OptionType":"CE"}]
-req_list_PE_strikeprice=[int(x/100)*100]
-req_list_CE_strikeprice=[int(x/100)*100]
-for i in range(1,25):
-    req_list_PE=req_list_PE+[{"Exch":"N","ExchType":"D","Symbol": main_str_pe+str(int(x/100)*100-i*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100-i*100),"OptionType":"PE"}] 
-    req_list_PE_strikeprice=req_list_PE_strikeprice+[int(x/100)*100-i*100]
-    req_list_CE=req_list_CE+[{"Exch":"N","ExchType":"D","Symbol":main_str_ce+str(int(x/100)*100+i*100)+".00","Expiry":expiry,"StrikePrice":str(int(x/100)*100+i*100),"OptionType":"CE"}] 
-    req_list_CE_strikeprice=req_list_CE_strikeprice+[int(x/100)*100+i*100]
-live_PE=Client.fetch_market_feed(req_list_PE)
-live_PE_lastrate=[]
-live_CE_lastrate=[]
-live_CE = Client.fetch_market_feed(req_list_CE)
-for j in range(0,25):
-    live_PE_lastrate=live_PE_lastrate+[live_PE['Data'][j]['LastRate']]
-    live_CE_lastrate = live_CE_lastrate+[live_CE['Data'][j]['LastRate']]
-CE_index_strikeprice=np.argmin(np.abs(np.array(live_CE_lastrate)-115))
-PE_index_strikeprice=np.argmin(np.abs(np.array(live_PE_lastrate)-115))
-CE_upper=req_list_CE_strikeprice[CE_index_strikeprice]
-PE_lower=req_list_PE_strikeprice[PE_index_strikeprice]
-strategy=strategies(user="@gmail.com", passw="PASSWORD", dob="YYYYMMDD",cred=cred)
-strategy.iron_fly("NIFTY",["15000","15200"],"15100","75","20210610","I")
+lower_sripcode=str(int(script[script['FullName']==main_str_name_format+strategy_lower_strikeprice+'.00']['Scripcode']))
+upper_scripcode=str(int(script[script['FullName']==main_str_name_format+strategy_upper_strikeprice+'.00']['Scripcode']))
+on_scripcode=str(int(script[script['FullName']==main_str_name_format+strategy_on_strikeprice+'.00']['Scripcode']))
+test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+Client.place_order(test_order)
+test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=upper_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+Client.place_order(test_order)
+test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=50,price=0,is_intraday=False,atmarket=True)
+Client.place_order(test_order)
+awesome=Client.positions()
+for i in range(0,len(awesome)):
+    if awesome[i]['ScripCode'] == int(lower_sripcode):
+        lower_lastrate=awesome[i]['LTP']
+    elif awesome[i]['ScripCode'] == int(upper_scripcode):
+        upper_lastrate=awesome[i]['LTP']
+    elif awesome[i]['ScripCode'] == int(on_scripcode):
+        on_lastrate=awesome[i]['LTP']
+# if all orders are placed we should not get error in the next 2 lines
+lower_breakeven=strategy_lower_strikeprice+(lower_lastrate+upper_lastrate-2*on_lastrate)
+upper_breakeven=strategy_upper_strikeprice-(lower_lastrate+upper_lastrate-2*on_lastrate)
+#%%
+if hole==0:
+    scripcode_ce=str(int(script[script['FullName']==main_str_format+'CE '+strategy_on_strikeprice+'.00']['Scripcode']))
+    while True:
+        req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]
+        a=Client.fetch_market_feed(req_list_)
+        x=a['Data'][0]['LastRate']
+        if x >=upper_breakeven-5:
+            #adjustment begins with a sell order at (on_strikeprice pe)
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+            Client.place_order(test_order)
+
+        elif x<=lower_breakeven+5:
+            #adjustment begins with a sell order at (on_strikeprice ce)
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=scripcode_ce, quantity=25,price=0,is_intraday=False,atmarket=True)
+            Client.place_order(test_order)
+elif hole==1:
+    scripcode_pe=str(int(script[script['FullName']==main_str_format+'PE '+strategy_on_strikeprice+'.00']['Scripcode']))
+    while True:
+        req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]
+        a=Client.fetch_market_feed(req_list_)
+        x=a['Data'][0]['LastRate']
+        if x >=upper_breakeven-5:
+            #adjustment begins with a sell order at (on_strikeprice pe)
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=, quantity=25,price=0,is_intraday=False,atmarket=True)
+            Client.place_order(test_order)
+
+        elif x<=lower_breakeven+5:
+            #adjustment begins with a sell order at (on_strikeprice ce)
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+            Client.place_order(test_order)
+        
+# need to write exit of the strategy
+
