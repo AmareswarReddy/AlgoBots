5846d5afa8895dd393d78705f89add14101da06b
PesalaVinayKumarReddy <88147078+PesalaVinayKumar@users.noreply.github.com>
2021-09-05 13:21:20 +0530

    new way to find option price at any strike price, any expiry, and at any volatility

---
+++ Options Strategies/option_dtm.py
@@ -0,0 +1,4 @@
+#import banknifty future data for 1 day (1min interval)
+#import banknifty greeks for all strikeprices
+#manual check on banknifty greeks for different strikeprices mainly gamma
+# Black Scholes formula implimentation. ref on youtube 
