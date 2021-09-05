002a53b7a5c1286c270107a2bafc21fd09c812f7
PesalaVinayKumarReddy <88147078+PesalaVinayKumar@users.noreply.github.com>
2021-09-05 15:06:13 +0530

    predict option price

--- Options Strategies/option_dtm.py
+++ Options Strategies/option_dtm.py
@@ -1,4 +1,6 @@
 #import banknifty future data for 1 day (1min interval)
 #import banknifty greeks for all strikeprices
 #manual check on banknifty greeks for different strikeprices mainly gamma
-# Black Scholes formula implimentation. ref on youtube 
+# Black Scholes formula implimentation. ref:  youtube series: khan academy black scholes formula
+
+ 
