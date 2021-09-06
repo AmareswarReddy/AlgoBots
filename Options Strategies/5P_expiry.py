6a8bf9de9858e59c98b4715ca1059a9cf535f336
PesalaVinayKumarReddy <88147078+PesalaVinayKumar@users.noreply.github.com>
2021-09-06 23:08:15 +0530

    exit at defined profit is added.

--- Options Strategies/5P_expiry.py
+++ Options Strategies/5P_expiry.py
@@ -1,4 +1,7 @@
 #%%
+
+# works only when you don't have positions of banknifty other than strategy based banknifty positions 
+# if you take banknifty positions anyway. Please look at the lines from 72 in the code 
 import numpy as np
 import pandas as pd
 from time import sleep
@@ -64,7 +67,16 @@ test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=
 Client.place_order(test_order)
 test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=50,price=0,is_intraday=False,atmarket=True)
 Client.place_order(test_order)
+sleep(5)
 awesome=Client.positions()
+potential_profit=0
+for i in range(0,len(awesome)):
+    if awesome[i]['ScripName'][:22] == main_str_format :
+        buy_avg=awesome[i]['BuyAvgRate']
+        sell_avg=awesome[i]['SellAvgRate']
+        buy_qty=awesome[i]['BuyQty']
+        sell_qty = awesome[i]['SellQty']
+        potential_profit = potential_profit + (buy_avg+sell_avg)*(sell_qty-buy_qty)
 for i in range(0,len(awesome)):
     if awesome[i]['ScripCode'] == int(lower_sripcode):
         lower_lastrate=awesome[i]['LTP']
@@ -84,7 +96,29 @@ if hole==0:
         req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]
         a=Client.fetch_market_feed(req_list_)
         x=a['Data'][0]['LastRate']
-        if x >=upper_breakeven-5:
+        profit=0
+        for i in range(0,len(awesome)):
+            if awesome[i]['ScripName'][:22] == main_str_format :
+                buy_avg=awesome[i]['BuyAvgRate']
+                sell_avg=awesome[i]['SellAvgRate']
+                buy_qty=awesome[i]['BuyQty']
+                sell_qty = awesome[i]['SellQty']
+                LTP=awesome[i]['LTP']
+                profit = profit + (buy_avg+sell_avg)*(sell_qty-buy_qty)+(buy_qty-sell_qty)*LTP
+        if profit>=potential_profit*0.75:
+            #square off all the positions
+            brk=1
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+            Client.place_order(test_order)
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=upper_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+            Client.place_order(test_order)
+            test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=50,price=0,is_intraday=False,atmarket=True)
+            Client.place_order(test_order)
+            test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+            Client.place_order(test_order)
+            break
+
+        if x >= upper_breakeven-5:
             #adjustment begins with a sell order at (on_strikeprice pe)
             test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
             Client.place_order(test_order)
@@ -92,7 +126,28 @@ if hole==0:
                 req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]
                 a=Client.fetch_market_feed(req_list_)
                 x=a['Data'][0]['LastRate']
-                if x<=SL_criteria_lower:
+                profit=0
+                for i in range(0,len(awesome)):
+                    if awesome[i]['ScripName'][:22] == main_str_format :
+                        buy_avg=awesome[i]['BuyAvgRate']
+                        sell_avg=awesome[i]['SellAvgRate']
+                        buy_qty=awesome[i]['BuyQty']
+                        sell_qty = awesome[i]['SellQty']
+                        LTP=awesome[i]['LTP']
+                        profit = profit + (buy_avg+sell_avg)*(sell_qty-buy_qty)+(buy_qty-sell_qty)*LTP
+                if profit>=potential_profit*0.75:
+                    #square off all the positions
+                    brk=1
+                    test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=upper_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=50,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    break
+                elif x<=SL_criteria_lower:
                     brk=1
                     #square off all the positions
                     test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
@@ -115,7 +170,28 @@ if hole==0:
                 req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]
                 a=Client.fetch_market_feed(req_list_)
                 x=a['Data'][0]['LastRate']
-                if x>=SL_criteria_upper:
+                profit=0
+                for i in range(0,len(awesome)):
+                    if awesome[i]['ScripName'][:22] == main_str_format :
+                        buy_avg=awesome[i]['BuyAvgRate']
+                        sell_avg=awesome[i]['SellAvgRate']
+                        buy_qty=awesome[i]['BuyQty']
+                        sell_qty = awesome[i]['SellQty']
+                        LTP=awesome[i]['LTP']
+                        profit = profit + (buy_avg+sell_avg)*(sell_qty-buy_qty)+(buy_qty-sell_qty)*LTP
+                if profit>=potential_profit*0.75:
+                    #square off all the positions
+                    brk=1
+                    test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=upper_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=50,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    break
+                elif x>=SL_criteria_upper:
                     brk=1
                     #square off all the positions
                     test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
@@ -143,7 +219,28 @@ elif hole==1:
                 req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]
                 a=Client.fetch_market_feed(req_list_)
                 x=a['Data'][0]['LastRate']
-                if x<=SL_criteria_lower:
+                profit=0
+                for i in range(0,len(awesome)):
+                    if awesome[i]['ScripName'][:22] == main_str_format :
+                        buy_avg=awesome[i]['BuyAvgRate']
+                        sell_avg=awesome[i]['SellAvgRate']
+                        buy_qty=awesome[i]['BuyQty']
+                        sell_qty = awesome[i]['SellQty']
+                        LTP=awesome[i]['LTP']
+                        profit = profit + (buy_avg+sell_avg)*(sell_qty-buy_qty)+(buy_qty-sell_qty)*LTP
+                if profit>=potential_profit*0.75:
+                    #square off all the positions
+                    brk=1
+                    test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=upper_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=50,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    break
+                elif x<=SL_criteria_lower:
                     brk=1
                     #square off all the positions
                     test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
@@ -166,7 +263,28 @@ elif hole==1:
                 req_list_=[{"Exch":"N","ExchType":"C","Symbol":"BANKNIFTY","Scripcode":"999920005","OptionType":"EQ"}]
                 a=Client.fetch_market_feed(req_list_)
                 x=a['Data'][0]['LastRate']
-                if x>=SL_criteria_upper:
+                profit=0
+                for i in range(0,len(awesome)):
+                    if awesome[i]['ScripName'][:22] == main_str_format :
+                        buy_avg=awesome[i]['BuyAvgRate']
+                        sell_avg=awesome[i]['SellAvgRate']
+                        buy_qty=awesome[i]['BuyQty']
+                        sell_qty = awesome[i]['SellQty']
+                        LTP=awesome[i]['LTP']
+                        profit = profit + (buy_avg+sell_avg)*(sell_qty-buy_qty)+(buy_qty-sell_qty)*LTP
+                if profit>=potential_profit*0.75:
+                    #square off all the positions
+                    brk=1
+                    test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=upper_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=50,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code=on_scripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
+                    Client.place_order(test_order)
+                    break
+                elif x>=SL_criteria_upper:
                     brk=1
                     #square off all the positions
                     test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code=lower_sripcode, quantity=25,price=0,is_intraday=False,atmarket=True)
