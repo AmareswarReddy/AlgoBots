1e4af7e77caddab9fc90cdc48e9a2bba16555064
PesalaVinayKumar <partyofwizard@gmail.com>
2021-09-07 20:51:07 +0530

    simplified explanation

--- Options Strategies/option_dtm.py
+++ Options Strategies/option_dtm.py
@@ -18,12 +18,12 @@ let the volatility is found to be 0.25
 we take this as 100 % and we keep finding the volatility on every minute basis.
 eg: let the time be 9:20AM and we found the volatility be 0.50
       we take 0.25 as the reference and we take 0.5 as 200 percent.
-      if the volatility be 200 percent as the highest volatility for the day. we consider 200 percent as our max volatility for that perticular day.
+      if the volatility be 200 percent as the highest volatility for the day. we consider 200 percent as our max volatility for that perticular day like thursday or friday etc.,.
 we will do the same for all the strike prices and we take those 2 strike prices at which stragle strategy could be formed when we run the strangle code.
 by taking those two strike prices. we see combined volatility and we start strangle when the volatility meets our requirements or when it hits max volatility for that perticular day.
 
 As strangle is not a good idea to be deployed after 12 o clock. So, when the volatility meets our condition before 12 o clock, we deploy our strat. Else we stop the program and look for other possible strategies
- 
+
 
 """
 
