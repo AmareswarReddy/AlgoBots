53d55ea534020991f30523aef3f06076f7136455
Amareswar Reddy <amareshnlr@gmail.com>
2021-09-08 18:46:34 +0530

    Create Black-Scholes.py

---
+++ Options Strategies/Black-Scholes.py
@@ -0,0 +1,11 @@
+# Steps to install the library
+
+# Step 1 : Open Anaconda
+# Step 2 : If multiple versions of Python or mutiple Virtual Python Environments are installed, activate the one you are using
+
+#Info : Suppose Python Virtual Env name is "py38", then run (conda activate py38); 
+
+# Step 3: Install the SWIG Dependency; Run (conda install swig)
+# Step 4: Install the Library; Run (pip install py_vollib)
+
+
