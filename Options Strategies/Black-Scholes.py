168bc12fa8d1865a70e6fa04db5096d858ebf3b2
PesalaVinayKumar <partyofwizard@gmail.com>
2021-09-09 01:50:51 +0530

    now, time is calculated by the code itself. just need to give no. of holidays ahead for the next expiry

--- Options Strategies/Black-Scholes.py
+++ Options Strategies/Black-Scholes.py
@@ -11,7 +11,16 @@
 #%%
 from py_vollib import black_scholes
 from py_vollib.black_scholes import implied_volatility
-
+from datetime import datetime,date
+holidays=int(input('enter number of holidays between expiry date and today date: '))
+today=str(datetime.today()).split()[0]
+expiry = "20210910"          # already defined in strangle strategy
+t_day=date(int(today[:4]),int(today[5:7]),int(today[8:10]))
+e_day = date(int(expiry[:4]),int(expiry[4:6]),int(expiry[6:8]))
+days_left=e_day-t_day
+T_zero=(int(str(days_left).split()[0])-holidays)*6.25
+now=datetime.now()
+time=now.strftime('%H %M')
 #price (float) – the Black-Scholes option price
 #S (float) – underlying asset price
 #K (float) – strike price
@@ -19,19 +28,28 @@ from py_vollib.black_scholes import implied_volatility
 #r (float) – risk-free interest rate
 #flag (str) – ‘c’ or ‘p’ for call or put.
 
-
 #For Futures Interest rate is 0%. So r should be Zero for Index such as BankNifty
+
+var=15-int(time.split()[0])+(30-int(time.split()[1]))/60
+#considering a day has only 6 hours 15 min (Total trading hours from 9:15am to 3:30pm)
+if var>0 and var<=6.5:
+    T_one= var
+elif 15-int(time.split()[0])>6.5:
+    T_one=6.5
+else :
+    T_one = 0
 
 # Test Values
 price = 115.6
 S = 36700
 K = 36900
-#considering a day has only 6 hours 15 min (total trading hours from 9:15am to 3:30pm)
-T = [12,30] #12 hours and 30 min
-t=(T[0]+T[1]/60)/(365*6.25)
-t = 0.1
+t=(T_one+T_zero)/(261*6.25)
 r = 0
 flag = 'c'
 
 iv = implied_volatility.implied_volatility(price=price, S=S, K=K, t=t, r=r, flag=flag)
+# looking at volatility in percentage
+iv = iv*100
 print(iv)
+
+# %%
