f84108a23dca98f17ebdb3401369dad416884705
PesalaVinayKumar <partyofwizard@gmail.com>
2022-01-15 23:14:00 +0530

    getoptiondata if not on the sys

--- Options Strategies/2plus1_Strangle.py
+++ Options Strategies/2plus1_Strangle.py
@@ -1,7 +1,9 @@
 #%%
 import json
 from os import error
+from datetime import datetime
 import numpy as np
+import requests
 #eg: optionchain=present_expiry[p_keys[0]]['optionchaindata']
 #eg: strikeprice= 35500
 alpha1=2 #call_ltp>=alpha1*put_ltp
@@ -21,9 +23,66 @@ def putprice(optionchain,strikeprice):
     price=float(a[a['Strikes']==strikeprice]['PutLTP'])
     return price
 
+def getOptionData(symbol = 'NIFTY', fromDate = '2018-12-25', fromTime = '09:20:00', toDate = '2018-12-25', toTime='15:35:00', expiry='02DEC2021' ):
+    baseURL = 'https://opstra.definedge.com/api/optionsimulator/optionchain/'
+    currentDate = fromDate + " "+ fromTime
+    datetime_object_current = datetime.strptime(currentDate, '%Y-%m-%d %X')
+    timestamp_current = int(datetime.timestamp(datetime_object_current))
+    #print(timestamp_current)
+    fivemin_timestamp = 300
+    endDate = toDate + " "+ toTime
+    datetime_object_endDate = datetime.strptime(endDate, '%Y-%m-%d %X')
+    timestamp_end = int(datetime.timestamp(datetime_object_endDate))
+    #print(timestamp_end)
+    final_data = {}
+    while (timestamp_current < timestamp_end):
+        dt_object = datetime.fromtimestamp(timestamp_current)
+        
+        only_time = dt_object.time()
+        timeparts = str(only_time).split(":")
+        if (9 <= int(timeparts[0]) <= 15):
+            #print(only_time)
+            #url = baseURL + str(timestamp_current)+"&"+symbol+"&"+(datetime.fromtimestamp(timestamp_current).strftime('%d%b%Y')).upper()
+            url = baseURL + str(timestamp_current)+"&"+symbol+"&"+ expiry
+            
+            #print(url)
+            tempData = downloaddata(url)
+            if tempData is not None:
+                final_data[str(dt_object)] = tempData
+                #print(dt_object)
+                #print(url)
+        timestamp_current = timestamp_current + fivemin_timestamp    
+    return final_data
 
-def readJson(symbol, fromDate, toDate,expiry):
+def downloaddata(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)",
+        "Accept": "*/*"
+    }
+    url = url
+    r = requests.get(url, headers=headers)
+    
+    if(r.status_code == 200):
+        json_data = json.loads(r.content)
+        return json_data;
+    else:
+        return None;
+    
+def jsonDump(symbol, fromDate, toDate, expiry,data):
+    # the json file where the output must be stored
     fileName = symbol+"_"+str(fromDate)+"_"+str(toDate)+"_"+str(expiry)+'.json'
+    out_file = open(fileName, "w")
+
+    json.dump(data, out_file, indent = 6)
+
+    out_file.close()
+
+def readJson(symbol, fromDate, toDate,expiry, fromTime='09:20:00', toTime='15:35:00'):
+    fileName = symbol+"_"+str(fromDate)+"_"+str(toDate)+"_"+str(expiry)+'.json'
+    if  not os.path.isfile(fileName):
+        print("Downloading Data")
+        data = getOptionData(symbol, fromDate,fromTime, toDate,toTime, expiry)
+        jsonDump(symbol,  fromDate,toDate,expiry, data)
     f = open(fileName)
     # returns JSON object as
     # a dictionary
@@ -91,7 +150,7 @@ ce_positions={p_keys[start]:[CE_upper,call_price]}
 pe_positions={p_keys[start]:[PE_lower,put_price]}
 for i in range(len(p_keys)):
     spot=spot+[present_expiry[p_keys[i]]['spotPrice']]
-    print(booked_profit)
+    #print(booked_profit)
     Current_CE_strikeprice=ce_positions[list(ce_positions.keys())[-1]][0]
     Current_PE_strikeprice=pe_positions[list(pe_positions.keys())[-1]][0]
     try:
@@ -132,7 +191,7 @@ for i in range(len(p_keys)):
                     modified_strangle_tracker = modified_strangle_tracker + 1
         elif modified_strangle_tracker%2 == 1:
                     CE_index_strikeprice = np.argmin(np.abs(np.array(live_CE_lastrate)- put_ltp))
-                    print(CE_index_strikeprice)
+                    #print(CE_index_strikeprice)
                     call_price = callprice(optionchain=present_expiry[p_keys[i]]['optionchaindata'],strikeprice=req_list_CE_strikeprice[CE_index_strikeprice])
                     ce_positions[p_keys[i]] = [req_list_CE_strikeprice[CE_index_strikeprice],call_price]
                     booked_profit = booked_profit+ce_positions[list(ce_positions.keys())[-2]][1]-call_ltp
@@ -165,7 +224,7 @@ for i in range(len(p_keys)):
             
         elif modified_strangle_tracker%2 == 1:
                     PE_index_strikeprice = np.argmin(np.abs(np.array(live_PE_lastrate)-call_ltp))
-                    print(PE_index_strikeprice)
+                    #print(PE_index_strikeprice)
                     put_price = putprice(optionchain=present_expiry[p_keys[i]]['optionchaindata'],strikeprice=req_list_PE_strikeprice[PE_index_strikeprice])
                     pe_positions[p_keys[i]] = [req_list_PE_strikeprice[PE_index_strikeprice],put_price]
                     booked_profit = booked_profit+pe_positions[list(pe_positions.keys())[-2]][1]-put_ltp
