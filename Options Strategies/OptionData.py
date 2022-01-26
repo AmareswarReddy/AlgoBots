f6920f30ca3724119f3fe480bf2b7fda2a99bf6b
Amareswar Reddy <amareshnlr@gmail.com>
2022-01-26 20:13:09 +0530

    Update OptionData.py

--- Options Strategies/OptionData.py
+++ Options Strategies/OptionData.py
@@ -1,5 +1,9 @@
+
+
+#%%
 import requests
 import json
+from datetime import datetime
 def downloaddata(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)",
@@ -16,4 +20,18 @@ def downloaddata(url):
 
 fiidii_data_url = "https://opstra.definedge.com/api/fiidiidata"
 fiidii_data = downloaddata(fiidii_data_url)
-print(fiidii_data)
+fiidii_data_daily = fiidii_data['daily']
+#print(fiidii_data_daily)
+fiidii_data_monthly = fiidii_data['monthly']
+#print(fiidii_data_monthly)
+
+
+openInterest_data_url = "https://opstra.definedge.com/api/openinterest/fiioidata/30"
+openInterest_data = downloaddata(openInterest_data_url)
+openInterest_latest_data =  openInterest_data['latestdata']
+final_OI_data = {}
+#print(openInterest_latest_data)
+for oi_data in openInterest_latest_data:
+    #print(oi_data['symname'])
+    final_OI_data[oi_data['symname']] = oi_data
+print(final_OI_data)
