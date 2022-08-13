3b78393cdd21c44014fdc3c12a1f9ad999cff23f
Amareswar Reddy <amareshnlr@gmail.com>
2022-08-13 22:42:38 +0530

    opstra
    opstra

---
+++ Options Strategies/test_opstra_chain.py
@@ -0,0 +1,102 @@
+#%%
+import json
+import os
+import numpy as np
+import requests
+import json
+from datetime import datetime
+#eg: optionchain=present_expiry[p_keys[0]]['optionchaindata']
+#eg: strikeprice= 35500
+import pandas as pd
+def callprice(optionchain,strikeprice):
+    a=pd.DataFrame(optionchain)
+    price=float(a[a['Strikes']==strikeprice]['CallLTP'])
+    return price
+
+def putprice(optionchain,strikeprice):
+    a=pd.DataFrame(optionchain)
+    price=float(a[a['Strikes']==strikeprice]['PutLTP'])
+    return price
+
+
+def readJson(symbol, fromDate, toDate,expiry, fromTime, toTime):
+    fileName = symbol+"_"+str(fromDate)+"_"+str(toDate)+"_"+str(expiry)+'.json'
+    if  not os.path.isfile(fileName):
+        print("Downloading Data")
+        data = getOptionData(symbol, fromDate,fromTime, toDate,toTime, expiry)
+        jsonDump(symbol,  fromDate,toDate,expiry, data)
+    f = open(fileName)
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+    f.close()
+    return data
+
+
+symbol = 'BANKNIFTY'
+fromDate = '2021-03-24'
+fromTime = '09:30:00'
+toDate = '2021-03-25'
+toTime='15:35:00'
+expiry = '25MAR2021'
+cookies = {'AWSALBAPP-0=':'AAAAAAAAAABviR24uzsHIckJ3hpAnutFyhHk4cLqeUqFY2vZSn60lnDWZF+e8D9djNdsWOwTo9/FBkg/bAovO74xdhWY+HeLPmyOvrmR6ZFWu82ZFvNOXAN7ZJfugBkjxKRPN3inWaPTCQ=='
+, 'AWSALBAPP-1':'_remove_', 'AWSALBAPP-2':'_remove_', 'AWSALBAPP-3':'_remove_', 'JSESSIONID':'B4DB9612F58A2787CCA625EBEC3AB087'}
+
+
+def getOptionData(symbol = 'NIFTY', fromDate = '2018-12-25', fromTime = '09:20:00', toDate = '2018-12-25', toTime='15:35:00', expiry='02DEC2021' ):
+    baseURL = 'https://opstra.definedge.com/api/optionsimulator/optionchain/'
+    currentDate = fromDate + " "+ fromTime
+    datetime_object_current = datetime.strptime(currentDate, '%Y-%m-%d %X')
+    timestamp_current = int(datetime.timestamp(datetime_object_current))
+    #print(timestamp_current)
+    fivemin_timestamp = 300
+    endDate = toDate + " "+ toTime
+    datetime_object_endDate = datetime.strptime(endDate, '%Y-%m-%d %X')
+    timestamp_end = int(datetime.timestamp(datetime_object_endDate))
+    #print(timestamp_end)
+    final_data = {}
+    while (timestamp_current < timestamp_end):
+        dt_object = datetime.fromtimestamp(timestamp_current)
+        
+        only_time = dt_object.time()
+        timeparts = str(only_time).split(":")
+        if (9 <= int(timeparts[0]) <= 15):
+            #print(only_time)
+            #url = baseURL + str(timestamp_current)+"&"+symbol+"&"+(datetime.fromtimestamp(timestamp_current).strftime('%d%b%Y')).upper()
+            url = baseURL + str(timestamp_current)+"&"+symbol+"&"+ expiry
+            
+            #print(url)
+            tempData = downloaddata(url)
+            if tempData is not None:
+                final_data[str(dt_object)] = tempData
+                #print(dt_object)
+                #print(url)
+        timestamp_current = timestamp_current + fivemin_timestamp    
+    return final_data
+
+def downloaddata(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)",
+        "Accept": "*/*"
+    }
+    url = url
+    r = requests.get(url, headers=headers, cookies= cookies)
+    
+    if(r.status_code == 200):
+        json_data = json.loads(r.content)
+        print(json_data)
+        return json_data
+    else:
+        return None
+    
+def jsonDump(symbol, fromDate, toDate, expiry,data):
+    # the json file where the output must be stored
+    fileName = symbol+"_"+str(fromDate)+"_"+str(toDate)+"_"+str(expiry)+'.json'
+    out_file = open(fileName, "w")
+
+    json.dump(data, out_file, indent = 6)
+
+    out_file.close()
+
+#dataJson = readJson(symbol, fromDate, toDate, expiry)
+present_expiry = readJson(symbol, fromDate, toDate, expiry, fromTime, toTime)
