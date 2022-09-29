fafcbd3b30217b2db013688946168fda99aae871
PesalaVinayKumar <partyofwizard@gmail.com>
2022-09-29 20:34:18 +0530

    update

--- Options Strategies/Lastest_findings/nifty_dollar_cointigration.py
+++ Options Strategies/Lastest_findings/nifty_dollar_cointigration.py
@@ -46,3 +46,63 @@ plt.plot(indicator)
 plt.show()
 
 # %%
+# for crude and dollar to be used to find cointegrated nifty
+import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import linear_model
+import pickle
+period='10000d'
+interval='1d'
+data = yf.download('^NSEI', period=period, interval=interval)
+nifty = data[:]
+data_dollar = yf.download('INR=X', period=period, interval=interval)
+data_crude = yf.download('CL=F', period=period, interval=interval)
+dollar = data_dollar[:]
+crude =data_crude[:]
+def clean_data(nifty,dollar,crude):
+    sp=np.array(nifty.index)
+    sc=np.array(dollar.index)
+    sl=np.array(crude.index)
+    k=np.intersect1d(np.array(sp),np.array(sc))
+    k2=np.intersect1d(np.array(sp),np.array(sl))
+    final_k=np.intersect1d(k,k2)
+    w=[]
+    for strike in sp:
+        if strike in final_k:
+            w=w+[True]
+        else:
+            w=w+[False]
+    v=[]
+    for strike in sc:
+        if strike in final_k:
+            v=v+[True]
+        else:
+            v=v+[False]
+    u=[]
+    for strike in sl:
+        if strike in final_k:
+            u=u+[True]
+        else:
+            u=u+[False]
+    nifty=nifty[w].copy()
+    dollar=dollar[v].copy()
+    crude=crude[u].copy()
+    return nifty,dollar,crude
+nifty,dollar,crude=clean_data(nifty,dollar,crude)
+x=np.zeros((len(dollar),2))
+x[:,0] = dollar['Close']
+x[:,1] = crude['Close']
+y_nifty= np.array(nifty[['Close']])
+regr = linear_model.LinearRegression()
+regr.fit(x,y_nifty)
+coint_nifty =  regr.predict(x)
+indicator=coint_nifty-y_nifty
+
+
+plt.plot(np.array(nifty['Close']))
+plt.plot(coint_nifty,'r')
+plt.show()
+plt.plot(indicator)
+plt.show()
