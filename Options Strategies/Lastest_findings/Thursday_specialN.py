da1b3a5e1bbddda3e951d3bb8e81129f1f07eaf7
PesalaVinayKumar <partyofwizard@gmail.com>
2022-10-17 09:30:30 +0530

    update

--- Options Strategies/Lastest_findings/Thursday_specialN.py
+++ Options Strategies/Lastest_findings/Thursday_specialN.py
@@ -42,7 +42,7 @@ def order_button(exclusive_strike,type,lots):
                 break
             except Exception :
                 pass
-        exclusive_strike=int(np.round(x/100)*100)
+        exclusive_strike=int(np.round(x/50)*50)
     else:
         while True:
             try :
@@ -113,6 +113,59 @@ def order_button(exclusive_strike,type,lots):
             prime_client['login'].place_order(test_order) 
     return exclusive_strike
 
+def buyer_adjustment_signal(c_strike,p_strike,exclusive_strike):
+    ind_time = datetime.now(timezone("Asia/Kolkata")).strftime('%Y-%m-%d %H:%M:%S.%f')
+    timer=int(ind_time[11:13])*60+int(ind_time[14:16])
+    #d=pd.DataFrame(prime_client['login'].positions())
+    #k=d[d['NetQty']!=0]
+    #j=[]
+    #for a in list(k['ScripName']):
+    #    j=j+[int(float(a.split(' ')[-1]))]
+    #k['StrikeRate']=j
+    #lastrates=k[k['StrikeRate']==exclusive_strike]['LTP']
+    #if len(lastrates)==2:
+    #    lastrate_sum=np.sum(lastrates)
+    lastrate_sum=np.sum(option_chain[option_chain['StrikeRate']==exclusive_strike]['LastRate'])
+    if (c_strike-p_strike>np.floor(2*lastrate_sum/50)*50):
+        return 1,np.floor(lastrate_sum/50)*50,0 
+    elif timer>925:
+        return 1,np.floor(lastrate_sum/50)*50,1
+    else:
+        return 0,0,0 #(change_of_buyside_strikes?, This_far_to_take_new_buy_side_positions, timer_trigger)
+
+def buyer_adjustments(exclusive_strike,k,c_strike,p_strike,buy_tron):
+    #k=(change_of_buyside_strikes, This_far_to_take_new_buy_side_positions, timer_trigger)
+    if k[0]==1:
+        c_strike_new=exclusive_strike+k[1]
+        p_strike_new=exclusive_strike-k[1]
+        if c_strike_new!=c_strike:
+            #enter new buyside positions
+            order_button(c_strike_new,'CE_B',buy_tron)
+            #exit old buyside positions
+            order_button(c_strike,'CE_S',buy_tron)
+        if p_strike_new!=p_strike:
+            #enter new buyside positions
+            order_button(p_strike_new,'PE_B',buy_tron)
+            #exit old buyside positions
+            order_button(p_strike,'PE_S',buy_tron)
+        c_strike=c_strike_new
+        p_strike=p_strike_new
+    return c_strike,p_strike
+
+def initial_trades(option_chain,x):
+    exclusive_strike=int(np.round(x/50)*50)
+    f=np.sum(option_chain[option_chain['StrikeRate']==int(np.round(x/50)*50)]['LastRate'])
+    factor=int(np.floor(f/50)*50)
+    c_strike=exclusive_strike+factor
+    p_strike=exclusive_strike-factor
+    #first buyside
+    order_button(p_strike,'PE_B',buy_tron)
+    order_button(c_strike,'CE_B',buy_tron)
+    #sell side
+    order_button(exclusive_strike,'PE_S',tron)
+    order_button(exclusive_strike,'CE_S',tron)
+    return exclusive_strike,c_strike,p_strike
+
 def good_to_go(prev_x,x):
     k=np.round(x/50)*50
     if (prev_x>k and k>x) or (prev_x<k and k<x):
@@ -144,15 +197,22 @@ def exit(option_chain,exclusive_strike):
 
 #%%
 #variables to be initialised
-client_name = 'vinathi'
+client_name = 'rahul'
 tron=int(input('enter the number of lots for trading (Eg 3):'))
+buy_tron=int(tron*1.3)
 prime_client=client_login(client=client_name)
 expiry_timestamps=prime_client['login'].get_expiry("N","NIFTY").copy()
 current_expiry_time_stamp_weekly=int(expiry_timestamps['Expiry'][0]['ExpiryDate'][6:19])
 option_chain=pd.DataFrame(prime_client['login'].get_option_chain("N","NIFTY",current_expiry_time_stamp_weekly)['Options'])
 prev_x=expiry_timestamps['lastrate'][0]['LTP']
-start=0
-exclusive_strike=0
+start=int(input('enter 0 if starting the strategy for the first time. else 1 :  '))
+if start==1:
+    exclusive_strike=int(input('enter exclusive strike :  '))
+    c_strike=int(input('enter call strike buyside :  '))
+    p_strike=int(input('enter put strike buyside :  '))
+elif start==0:
+    option_chain,x=data()
+    exclusive_strike,c_strike,p_strike=0,0,0
 #%%
 ind_time = datetime.now(timezone("Asia/Kolkata")).strftime('%Y-%m-%d %H:%M:%S.%f')
 while int(ind_time[11:13])*60+int(ind_time[14:16])<555 or int(ind_time[11:13])*60+int(ind_time[14:16])>885 :
@@ -163,8 +223,7 @@ while True:
     #x=int(input('--'))
     if start==0:
         if good_to_go(x=x,prev_x=prev_x)>0:
-            exclusive_strike=order_button(int(np.round(x/50)*50),'PE_S',tron)
-            exclusive_strike=order_button(int(np.round(x/50)*50),'CE_S',tron)
+            exclusive_strike,c_strike,p_strike=initial_trades(option_chain=option_chain,x=x)
             start=1
     if start==1:
         if change_of_strike(earlier_x=exclusive_strike,x=x)>1:
@@ -175,9 +234,8 @@ while True:
     if exit(option_chain,exclusive_strike)==1 and exclusive_strike!=0:
         order_button(exclusive_strike,'PE_B',tron)
         order_button(exclusive_strike,'CE_B',tron)   
-        break     
+        break   
+    k=buyer_adjustment_signal(c_strike,p_strike,exclusive_strike)  
+    c_strike,p_strike=buyer_adjustments(exclusive_strike,k,c_strike,p_strike,buy_tron)
     prev_x=x
-
-
-
 # %%
