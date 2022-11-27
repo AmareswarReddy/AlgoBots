05626034674cfaf62bcaf0e589564349101a1ef9
PesalaVinayKumar <partyofwizard@gmail.com>
2022-11-28 00:05:01 +0530

    update

--- Options Strategies/Lastest_findings/backtester/pso_tester.py
+++ Options Strategies/Lastest_findings/backtester/pso_tester.py
@@ -5,20 +5,18 @@ import pandas as pd
 import json
 from pyswarm import pso
 import os
-from numba import njit
 #data import
 list_of_data=os.listdir()
 train_data=[]
 test_data=[]
 for file_name in list_of_data:
     if 'json' in file_name:
-        if np.random.rand()<0.95:
+        if np.random.rand()<0.8:
             train_data=train_data+[pd.DataFrame(json.load(open(file_name)))]
         else:
             test_data=test_data+[pd.DataFrame(json.load(open(file_name)))]
 #%%
 #model
-@njit
 def pnl(data):
     b=list(data['indicator']>0)+[0]
     b[0]=0
@@ -30,13 +28,12 @@ def pnl(data):
     net_pnl=np.dot(buyer,np.array(data['lastrate']))+np.dot(seller,np.array(data['lastrate']))-trades*5
     return net_pnl
 
-@njit
 def indicator(w11,w12,w13,w14,c11,c12,c13,c14,outw11,outw12,outw13,outw14,outc11,outw21,outw22,outw23,outw24,outc21,data):
     #layer1
-    A1=np.tanh(w11*data['hightime']+c11)
-    A2=np.tanh(w12*data['oi_ratio']+c12)
-    A3=np.tanh(w13*data['rosetta_ratio']+c13)
-    A4=np.tanh(w14*data['rosetta']+c14)
+    A1=np.tanh(w11*np.array(data['hightime'])+c11)
+    A2=np.tanh(w12*np.array(data['oi_ratio'])+c12)
+    A3=np.tanh(w13*np.array(data['rosetta_ratio'])+c13)
+    A4=np.tanh(w14*np.array(data['rosetta'])+c14)
     layer11=outw11*A1+outw12*A2+outw13*A3+outw14*A4
     layer12=outw21*A1+outw22*A2+outw23*A3+outw24*A4
     output1=np.tanh(layer11+outc11)
@@ -46,7 +43,6 @@ def indicator(w11,w12,w13,w14,c11,c12,c13,c14,outw11,outw12,outw13,outw14,outc11
     return data
 
 # loss function
-@njit
 def lossfunc(x):
     # define data
     net_profit=0
@@ -60,12 +56,11 @@ def lossfunc(x):
 # optimisation
 lb=[-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5,-5]
 ub=list(np.array(lb)+10)
-xopt,fopt=pso(lossfunc,lb,ub,maxiter=100,swarmsize=500)
+xopt,fopt=pso(lossfunc,lb,ub,maxiter=80,swarmsize=1000)
 
 #%%
 #cross check plots
 fopt_test=[]
-@njit
 def lossfunc_test(x):
     net_profit=0
     # define data
@@ -77,9 +72,9 @@ def lossfunc_test(x):
     return -net_profit
 for item in xopt:
     fopt_test+=[lossfunc_test(item)]
-plt.plot(fopt)
+plt.plot(fopt/len(train_data))
 plt.show()
-plt.plot(fopt_test)
+plt.plot(fopt_test/len(test_data))
 plt.show()
 
 # %%
