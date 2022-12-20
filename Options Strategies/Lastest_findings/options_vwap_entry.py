d78008344e174a9495a1b39b39eb37bc41b50429
PesalaVinayKumar <partyofwizard@gmail.com>
2022-12-20 13:09:29 +0530

    update

---
+++ Options Strategies/Lastest_findings/options_vwap_entry.py
@@ -0,0 +1,247 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import json
+from time import sleep, strftime
+from py5paisa import FivePaisaClient
+from py5paisa.strategy import *
+from py5paisa.logging import log_response
+from datetime import datetime
+from datetime import date
+import requests
+from pytz import timezone 
+from cred import *
+from py5paisa.order import Basket_order
+def client_login(client):
+    import json
+    f = open ('credentials.json', "r")
+    creds = json.loads(f.read())
+    client_list={}
+    client_list[client]={'strategy':{},'login':{},'lots':{}}
+    vinathi_cred = creds[client]["keys"]
+    user = creds[client]["user"]
+    passw = creds[client]["passw"]
+    dob = creds[client]["dob"]
+    client_list[client]['strategy']=strategies(user=user, passw=passw, dob=dob,cred=vinathi_cred)
+    client_list[client]['login']=FivePaisaClient(email=user, passwd=passw, dob=dob,cred=vinathi_cred)
+    client_list[client]['login'].login()
+    client_list[client]['lots']=round((client_list[client]['login'].margin()[0]['AvailableMargin']-200000)/180000)
+    return client_list[client]
+#client_name=input('enter the client name Eg: vinathi,bhaskar '
+
+def order_button(exclusive_strike,type,lots):
+    sleep(0.5)
+    exchange='BANKNIFTY'
+    lot_size=25
+    max_lots_per_order=36
+    strike_difference=100
+    if exclusive_strike==0:
+        while True:
+            try :
+                expiry_timestamps=prime_client['login'].get_expiry("N",exchange).copy()
+                current_expiry_time_stamp_weekly=int(expiry_timestamps['Expiry'][0]['ExpiryDate'][6:19])
+                option_chain=pd.DataFrame(prime_client['login'].get_option_chain("N",exchange,current_expiry_time_stamp_weekly)['Options'])
+                x=expiry_timestamps['lastrate'][0]['LTP']
+                break
+            except Exception :
+                pass
+        exclusive_strike=int(np.round(x/strike_difference)*strike_difference)
+    else:
+        while True:
+            try :
+                expiry_timestamps=prime_client['login'].get_expiry("N",exchange).copy()
+                current_expiry_time_stamp_weekly=int(expiry_timestamps['Expiry'][0]['ExpiryDate'][6:19])
+                option_chain=pd.DataFrame(prime_client['login'].get_option_chain("N",exchange,current_expiry_time_stamp_weekly)['Options'])
+                x=expiry_timestamps['lastrate'][0]['LTP']
+                break
+            except Exception :
+                pass
+    if type=='CE_B':
+        already_placed=0
+        c_data=option_chain[option_chain['CPType']=='CE']
+        c_scrip=int(c_data[c_data['StrikeRate']==exclusive_strike]['ScripCode'])
+        temp2=lots
+        temp=int(temp2/max_lots_per_order)
+        end=temp2-temp*max_lots_per_order
+        test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code =c_scrip, quantity=lot_size*max_lots_per_order, price=0 ,is_intraday=False,remote_order_id="tag")
+        while temp>0:
+            status=prime_client['login'].place_order(test_order)
+            temp=temp-1 
+            if status['Message']=='Success':
+                already_placed+=max_lots_per_order
+            sleep(0.5)
+        if temp==0 and end!=0:
+            test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code =c_scrip, quantity=lot_size*end, price=0 ,is_intraday=False,remote_order_id="tag")
+            status=prime_client['login'].place_order(test_order)
+            if status['Message']=='Success':
+                already_placed+=end
+        yet_to_place=lots-already_placed 
+    if type=='PE_B':
+        already_placed=0
+        p_data=option_chain[option_chain['CPType']=='PE']
+        p_scrip=int(p_data[p_data['StrikeRate']==exclusive_strike]['ScripCode'])
+        temp2=lots
+        temp=int(temp2/max_lots_per_order)
+        end=temp2-temp*max_lots_per_order
+        test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code =p_scrip, quantity=lot_size*max_lots_per_order, price=0 ,is_intraday=False,remote_order_id="tag")
+        while temp>0:
+            status=prime_client['login'].place_order(test_order) 
+            temp=temp-1
+            if status['Message']=='Success':
+                already_placed+=max_lots_per_order
+            sleep(0.5)
+        if temp==0 and end!=0:
+            test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code =p_scrip, quantity=lot_size*end, price=0 ,is_intraday=False,remote_order_id="tag")
+            status=prime_client['login'].place_order(test_order) 
+            if status['Message']=='Success':
+                already_placed+=end
+        yet_to_place=lots-already_placed
+    if type=='CE_S':
+        already_placed=0
+        c_data=option_chain[option_chain['CPType']=='CE']
+        c_scrip=int(c_data[c_data['StrikeRate']==exclusive_strike]['ScripCode'])
+        temp2=lots
+        temp=int(temp2/max_lots_per_order)
+        end=temp2-temp*max_lots_per_order
+        test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code =c_scrip, quantity=lot_size*max_lots_per_order, price=0 ,is_intraday=False,remote_order_id="tag")
+        while temp>0:
+            status=prime_client['login'].place_order(test_order) 
+            temp=temp-1
+            if status['Message']=='Success':
+                already_placed+=max_lots_per_order
+            sleep(0.5)
+        if temp==0 and end!=0:
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code =c_scrip, quantity=lot_size*end, price=0 ,is_intraday=False,remote_order_id="tag")
+            status=prime_client['login'].place_order(test_order) 
+            if status['Message']=='Success':
+                already_placed+=end
+        yet_to_place=lots-already_placed
+    if type=='PE_S':
+        already_placed=0
+        p_data=option_chain[option_chain['CPType']=='PE']
+        p_scrip=int(p_data[p_data['StrikeRate']==exclusive_strike]['ScripCode'])
+        temp2=lots
+        temp=int(temp2/max_lots_per_order)
+        end=temp2-temp*max_lots_per_order
+        test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code =p_scrip, quantity=lot_size*max_lots_per_order, price=0 ,is_intraday=False,remote_order_id="tag")
+        while temp>0:
+            status=prime_client['login'].place_order(test_order) 
+            temp=temp-1
+            if status['Message']=='Success':
+                already_placed+=max_lots_per_order
+            sleep(0.5)
+        if temp==0 and end!=0:
+            test_order = Order(order_type='S',exchange='N',exchange_segment='D', scrip_code =p_scrip, quantity=lot_size*end, price=0 ,is_intraday=False,remote_order_id="tag")
+            status=prime_client['login'].place_order(test_order) 
+            if status['Message']=='Success':
+                already_placed+=end
+        yet_to_place=lots-already_placed
+    return exclusive_strike,yet_to_place
+
+def lots_drop(strike,side,yet_to_place):
+    k=yet_to_place
+    while yet_to_place>0:
+        sleep(1)
+        yet_to_place-=1
+        xx,pending=order_button(strike,side,yet_to_place)
+        if pending==0:
+            break
+    return k-yet_to_place
+
+def finalise_tron(p_strike,c_strike,tron):
+    p_strike,p_yet_to_place=order_button(p_strike,'PE_S',tron)
+    c_strike,c_yet_to_place=order_button(c_strike,'CE_S',tron)
+    if p_yet_to_place==0 and c_yet_to_place==0:
+        return tron
+    if p_yet_to_place!=0 and c_yet_to_place==0:
+        while True:
+            tron=tron-1
+            order_button(c_strike,'CE_B',1)
+            kkk,y_place=order_button(p_strike,'PE_S',tron)
+            if y_place==0:
+                break
+        return tron
+    if p_yet_to_place==0 and c_yet_to_place!=0:
+        while True:
+            tron=tron-1
+            order_button(p_strike,'PE_B',1)
+            kkk,y_place=order_button(c_strike,'CE_S',tron)
+            if y_place==0:
+                break
+        return tron
+    if p_yet_to_place!=0 and c_yet_to_place!=0:
+        return finalise_tron(p_strike=p_strike,c_strike=c_strike,tron=tron-1)
+
+
+def data(week):
+    exchange='BANKNIFTY'
+    while True:
+        expiry_timestamps=prime_client['login'].get_expiry("N",exchange).copy()
+        current_expiry_time_stamp_weekly=int(expiry_timestamps['Expiry'][week]['ExpiryDate'][6:19])
+        try :
+            expiry_timestamps=prime_client['login'].get_expiry("N",exchange).copy()
+            option_chain=pd.DataFrame(prime_client['login'].get_option_chain("N",exchange,current_expiry_time_stamp_weekly)['Options'])
+            x=expiry_timestamps['lastrate'][0]['LTP']
+            break
+        except Exception :
+            pass
+    return option_chain,x
+
+
+def options_vwap_json(option_chain,calloptions_vwap,putoptions_vwap):
+    ce_data=option_chain[option_chain['CPType']=='CE']
+    pe_data=option_chain[option_chain['CPType']=='PE']
+    c_lastrate=np.array(ce_data['LastRate'])
+    p_lastrate= np.array(pe_data['LastRate'])
+    c_volumes=  np.array(ce_data['Volume'])+0.01
+    p_volumes=  np.array(pe_data['Volume'])+0.01
+    prev_c_lastrate=    np.array(calloptions_vwap['LastRate'])
+    prev_p_lastrate=    np.array(putoptions_vwap['LastRate'])
+    prev_c_volumes=     np.array(calloptions_vwap['Volume'])+0.01
+    prev_p_volumes=     np.array(putoptions_vwap['Volume'])+0.01
+    c_net=np.multiply(c_volumes-prev_c_volumes,c_lastrate)
+    p_net=np.multiply(p_volumes-prev_p_volumes,p_lastrate)
+    call_vwap=np.multiply((c_net+np.multiply(prev_c_lastrate,prev_c_volumes)),1/c_volumes)
+    put_vwap=np.multiply((p_net+np.multiply(prev_p_lastrate,prev_p_volumes)),1/p_volumes)
+    calloptions_vwap=ce_data[['StrikeRate','LastRate','Volume']].copy()
+    putoptions_vwap=pe_data[['StrikeRate','LastRate','Volume']].copy()
+    calloptions_vwap['LastRate']=call_vwap    
+    calloptions_vwap['Volume']=calloptions_vwap['Volume']+0.01
+    putoptions_vwap['LastRate']=put_vwap    
+    putoptions_vwap['Volume']=putoptions_vwap['Volume']+0.01
+    return calloptions_vwap,putoptions_vwap
+
+#%%
+client_name = input('enter the client name: ')
+prime_client=client_login(client=client_name)
+option_chain,x=data(week=0)
+ce_data=option_chain[option_chain['CPType']=='CE']
+pe_data=option_chain[option_chain['CPType']=='PE']
+calloptions_vwap=ce_data[['StrikeRate','LastRate','Volume']].copy()
+putoptions_vwap=pe_data[['StrikeRate','LastRate','Volume']].copy()
+#start=int(input('enter 0 if starting the strategy for the first time, else 1 :  '))
+indicator=[]
+lastrate=[]
+#%%
+while True:
+    option_chain,x=data(week=0)
+    ce_data=option_chain[option_chain['CPType']=='CE']
+    pe_data=option_chain[option_chain['CPType']=='PE']
+    calloptions_vwap,putoptions_vwap=options_vwap_json(option_chain,calloptions_vwap,putoptions_vwap)
+    call_move=sum(np.sign(np.array(ce_data['LastRate']-calloptions_vwap['LastRate'])))
+    put_move= sum(np.sign(np.array(putoptions_vwap['LastRate']-pe_data['LastRate'])))
+    total=sum(np.abs(np.array(ce_data['LastRate']-calloptions_vwap['LastRate'])))+sum(np.abs(np.array(putoptions_vwap['LastRate']-pe_data['LastRate'])))
+    indicator=indicator+[(call_move+put_move)/(total)]
+    lastrate=lastrate+[x]
+    new={'lastrate':lastrate,'indicator':indicator}
+    print(indicator[-1])
+out_file = open('indicator.json', "w")
+json.dump(new, out_file, indent = 6)
+out_file.close()
+# %%
+fig, ax_left = plt.subplots()
+ax_right = ax_left.twinx()
+ax_left.plot(new['lastrate'][100:], color='blue')
+ax_right.plot(new['indicator'][100:], color='red')
+# %%
