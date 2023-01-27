fedf9b30934b810ed2159e06912a3be423b0e396
Amareswar Reddy <amareshnlr@gmail.com>
2023-01-27 19:09:13 +0530

    Updated the code

--- Options Strategies/Strangle_BackTest_Yearly.py
+++ Options Strategies/Strangle_BackTest_Yearly.py
@@ -14,17 +14,90 @@ days_in_strategy=4
 import pandas as pd
 import requests
 import json
+
+from datetime import datetime
+
+def getOptionData(symbol = 'NIFTY', fromDate = '2018-12-25', fromTime = '09:20:00', toDate = '2018-12-25', toTime='15:35:00', expiry='02DEC2021' ):
+    baseURL = 'https://opstra.definedge.com/api/optionsimulator/optionchain/'
+    currentDate = fromDate + " "+ fromTime
+    datetime_object_current = datetime.strptime(currentDate, '%Y-%m-%d %X')
+    timestamp_current = int(datetime.timestamp(datetime_object_current))
+    #print(timestamp_current)
+    fivemin_timestamp = 300
+    endDate = toDate + " "+ toTime
+    datetime_object_endDate = datetime.strptime(endDate, '%Y-%m-%d %X')
+    timestamp_end = int(datetime.timestamp(datetime_object_endDate))
+    #print(timestamp_end)
+    final_data = {}
+    while (timestamp_current < timestamp_end):
+        dt_object = datetime.fromtimestamp(timestamp_current)
+        
+        only_time = dt_object.time()
+        timeparts = str(only_time).split(":")
+        if (9 <= int(timeparts[0]) <= 15):
+            #print(only_time)
+            #url = baseURL + str(timestamp_current)+"&"+symbol+"&"+(datetime.fromtimestamp(timestamp_current).strftime('%d%b%Y')).upper()
+            url = baseURL + str(timestamp_current)+"&"+symbol+"&"+ expiry
+            
+            #print(url)
+            tempData = downloaddata(url)
+            if tempData is not None:
+                final_data[str(dt_object)] = tempData
+                #print(dt_object)
+                #print(url)
+        timestamp_current = timestamp_current + fivemin_timestamp    
+    return final_data
+
+  
+
+import requests
+import json
+cookies = {"JSESSIONID":"C6E096EA043605DCBE57418BDAAEA657"}
+def downloaddata(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)",
+        "Accept": "*/*"
+    }
+    url = url
+    r = requests.get(url, headers=headers, cookies= cookies)
+    
+    if(r.status_code == 200):
+        json_data = json.loads(r.content)
+        return json_data
+    else:
+        return None
+    
+def jsonDump(symbol, fromDate, toDate, expiry,data):
+    # the json file where the output must be stored
+    fileName = symbol+"_"+str(fromDate)+"_"+str(toDate)+"_"+str(expiry)+'.json'
+    out_file = open(fileName, "w")
+
+    json.dump(data, out_file, indent = 6)
+
+    out_file.close()
+
+
+def readJson(symbol, fromDate, toDate,expiry):
+    fileName = symbol+"_"+str(fromDate)+"_"+str(toDate)+"_"+str(expiry)+'.json'
+    f = open(fileName)
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+    f.close()
+    return data
+
+
 def getexpires():
     headers = {
         "User-Agent": "Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)",
         "Accept": "*/*"
     }
     url = 'https://opstra.definedge.com/api/optionsimulator/simulatorexpiries'
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, cookies= cookies)
     
     if(r.status_code == 200):
         json_data = json.loads(r.content)
-        return json_data;
+        return json_data
 expires_list = getexpires()
 
 def callprice(optionchain,strikeprice):
@@ -212,77 +285,5 @@ for i in range(0, len(expires_list)):
         print('Total profit booked:',profit[-1])
         df_writeData.loc[0 if pd.isnull(df_writeData.index.max()) else df_writeData.index.max() + 1] = [expiry, profit[-1]]
         df_writeData.to_excel('report.xlsx')
-# %%
-
-
-from datetime import datetime
-
-def getOptionData(symbol = 'NIFTY', fromDate = '2018-12-25', fromTime = '09:20:00', toDate = '2018-12-25', toTime='15:35:00', expiry='02DEC2021' ):
-    baseURL = 'https://opstra.definedge.com/api/optionsimulator/optionchain/'
-    currentDate = fromDate + " "+ fromTime
-    datetime_object_current = datetime.strptime(currentDate, '%Y-%m-%d %X')
-    timestamp_current = int(datetime.timestamp(datetime_object_current))
-    #print(timestamp_current)
-    fivemin_timestamp = 300
-    endDate = toDate + " "+ toTime
-    datetime_object_endDate = datetime.strptime(endDate, '%Y-%m-%d %X')
-    timestamp_end = int(datetime.timestamp(datetime_object_endDate))
-    #print(timestamp_end)
-    final_data = {}
-    while (timestamp_current < timestamp_end):
-        dt_object = datetime.fromtimestamp(timestamp_current)
-        
-        only_time = dt_object.time()
-        timeparts = str(only_time).split(":")
-        if (9 <= int(timeparts[0]) <= 15):
-            #print(only_time)
-            #url = baseURL + str(timestamp_current)+"&"+symbol+"&"+(datetime.fromtimestamp(timestamp_current).strftime('%d%b%Y')).upper()
-            url = baseURL + str(timestamp_current)+"&"+symbol+"&"+ expiry
-            
-            #print(url)
-            tempData = downloaddata(url)
-            if tempData is not None:
-                final_data[str(dt_object)] = tempData
-                #print(dt_object)
-                #print(url)
-        timestamp_current = timestamp_current + fivemin_timestamp    
-    return final_data
-
-  
-
-import requests
-import json
-def downloaddata(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)",
-        "Accept": "*/*"
-    }
-    url = url
-    r = requests.get(url, headers=headers)
-    
-    if(r.status_code == 200):
-        json_data = json.loads(r.content)
-        return json_data;
-    else:
-        return None;
-    
-def jsonDump(symbol, fromDate, toDate, expiry,data):
-    # the json file where the output must be stored
-    fileName = symbol+"_"+str(fromDate)+"_"+str(toDate)+"_"+str(expiry)+'.json'
-    out_file = open(fileName, "w")
-
-    json.dump(data, out_file, indent = 6)
-
-    out_file.close()
-
-
-def readJson(symbol, fromDate, toDate,expiry):
-    fileName = symbol+"_"+str(fromDate)+"_"+str(toDate)+"_"+str(expiry)+'.json'
-    f = open(fileName)
-    # returns JSON object as
-    # a dictionary
-    data = json.load(f)
-    f.close()
-    return data
 
 # %%
