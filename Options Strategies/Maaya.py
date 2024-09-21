45537f5143ce27ba42378ea39cd147cf34f5ec31
PesalaVinayKumar <partyofwizard@gmail.com>
2024-09-21 18:59:56 +0530

    update

--- Options Strategies/Maaya.py
+++ Options Strategies/Maaya.py
@@ -25,13 +25,9 @@ def client_login(client):
     client_list = {}
     client_list[client] = {'strategy': {}, 'login': {}, 'lots': {}}
     vinathi_cred = creds[client]["keys"]
-    user = creds[client]["user"]
-    passw = creds[client]["passw"]
-    dob = creds[client]["dob"]
-    client_list[client]['strategy'] = strategies(
-        user=user, passw=passw, dob=dob, cred=vinathi_cred)
     client_list[client]['login'] = FivePaisaClient(cred=vinathi_cred)
-    client_list[client]['login'].get_totp_session('', '', '')
+    client_list[client]['login'].get_totp_session(
+        '56936208', '653249', '001273')
     return client_list[client]
 # client_name=input('enter the client name Eg: vinathi,bhaskar '
 
@@ -76,17 +72,15 @@ def order_button(exclusive_strike, type, lots, week):
         temp2 = lots
         temp = int(temp2/max_lots_per_order)
         end = temp2-temp*max_lots_per_order
-        test_order = Order(order_type='B', exchange='N', exchange_segment='D', scrip_code=c_scrip,
-                           quantity=lot_size*max_lots_per_order, price=0, is_intraday=False, remote_order_id="tag")
         while temp > 0:
-            status = prime_client['login'].place_order(test_order)
+            status = prime_client['login'].place_order(
+                OrderType='B', Exchange='N', ExchangeType='D', ScripCode=c_scrip, Qty=lot_size*max_lots_per_order, Price=0)
             temp = temp-1
             if status['Message'] == 'Success':
                 already_placed += max_lots_per_order
         if temp == 0 and end != 0:
-            test_order = Order(order_type='B', exchange='N', exchange_segment='D', scrip_code=c_scrip,
-                               quantity=lot_size*end, price=0, is_intraday=False, remote_order_id="tag")
-            status = prime_client['login'].place_order(test_order)
+            status = prime_client['login'].place_order(
+                OrderType='B', Exchange='N', ExchangeType='D', ScripCode=c_scrip, Qty=lot_size*end, Price=0)
             if status['Message'] == 'Success':
                 already_placed += end
         yet_to_place = lots-already_placed
@@ -98,17 +92,15 @@ def order_button(exclusive_strike, type, lots, week):
         temp2 = lots
         temp = int(temp2/max_lots_per_order)
         end = temp2-temp*max_lots_per_order
-        test_order = Order(order_type='B', exchange='N', exchange_segment='D', scrip_code=p_scrip,
-                           quantity=lot_size*max_lots_per_order, price=0, is_intraday=False, remote_order_id="tag")
         while temp > 0:
-            status = prime_client['login'].place_order(test_order)
+            status = prime_client['login'].place_order(
+                OrderType='B', Exchange='N', ExchangeType='D', ScripCode=p_scrip, Qty=lot_size*max_lots_per_order, Price=0)
             temp = temp-1
             if status['Message'] == 'Success':
                 already_placed += max_lots_per_order
         if temp == 0 and end != 0:
-            test_order = Order(order_type='B', exchange='N', exchange_segment='D', scrip_code=p_scrip,
-                               quantity=lot_size*end, price=0, is_intraday=False, remote_order_id="tag")
-            status = prime_client['login'].place_order(test_order)
+            status = prime_client['login'].place_order(
+                OrderType='B', Exchange='N', ExchangeType='D', ScripCode=p_scrip, Qty=lot_size*end, Price=0)
             if status['Message'] == 'Success':
                 already_placed += end
         yet_to_place = lots-already_placed
@@ -120,17 +112,15 @@ def order_button(exclusive_strike, type, lots, week):
         temp2 = lots
         temp = int(temp2/max_lots_per_order)
         end = temp2-temp*max_lots_per_order
-        test_order = Order(order_type='S', exchange='N', exchange_segment='D', scrip_code=c_scrip,
-                           quantity=lot_size*max_lots_per_order, price=0, is_intraday=False, remote_order_id="tag")
         while temp > 0:
-            status = prime_client['login'].place_order(test_order)
+            status = prime_client['login'].place_order(
+                OrderType='S', Exchange='N', ExchangeType='D', ScripCode=c_scrip, Qty=lot_size*max_lots_per_order, Price=0)
             temp = temp-1
             if status['Message'] == 'Success':
                 already_placed += max_lots_per_order
         if temp == 0 and end != 0:
-            test_order = Order(order_type='S', exchange='N', exchange_segment='D', scrip_code=c_scrip,
-                               quantity=lot_size*end, price=0, is_intraday=False, remote_order_id="tag")
-            status = prime_client['login'].place_order(test_order)
+            status = prime_client['login'].place_order(
+                OrderType='S', Exchange='N', ExchangeType='D', ScripCode=c_scrip, Qty=lot_size*end, Price=0)
             if status['Message'] == 'Success':
                 already_placed += end
         yet_to_place = lots-already_placed
@@ -142,38 +132,45 @@ def order_button(exclusive_strike, type, lots, week):
         temp2 = lots
         temp = int(temp2/max_lots_per_order)
         end = temp2-temp*max_lots_per_order
-        test_order = Order(order_type='S', exchange='N', exchange_segment='D', scrip_code=p_scrip,
-                           quantity=lot_size*max_lots_per_order, price=0, is_intraday=False, remote_order_id="tag")
         while temp > 0:
-            status = prime_client['login'].place_order(test_order)
+            status = prime_client['login'].place_order(
+                OrderType='S', Exchange='N', ExchangeType='D', ScripCode=p_scrip, Qty=lot_size*max_lots_per_order, Price=0)
             temp = temp-1
             if status['Message'] == 'Success':
                 already_placed += max_lots_per_order
         if temp == 0 and end != 0:
-            test_order = Order(order_type='S', exchange='N', exchange_segment='D', scrip_code=p_scrip,
-                               quantity=lot_size*end, price=0, is_intraday=False, remote_order_id="tag")
-            status = prime_client['login'].place_order(test_order)
+            status = prime_client['login'].place_order(
+                OrderType='S', Exchange='N', ExchangeType='D', ScripCode=p_scrip, Qty=lot_size*end, Price=0)
             if status['Message'] == 'Success':
                 already_placed += end
         yet_to_place = lots-already_placed
     return exclusive_strike, yet_to_place
 
 
-def data(week):
-    exchange = 'NIFTY'
-    while True:
-        try:
-            expiry_timestamps = prime_client['login'].get_expiry(
-                "N", exchange).copy()
-            current_expiry_time_stamp_weekly = int(
-                expiry_timestamps['Expiry'][week]['ExpiryDate'][6:19])
-            option_chain = pd.DataFrame(prime_client['login'].get_option_chain(
-                "N", exchange, current_expiry_time_stamp_weekly)['Options'])
-            x = expiry_timestamps['lastrate'][0]['LTP']
-            break
-        except Exception:
-            pass
-    return option_chain, x
+def rosetta(option_chain):
+    ce_data = option_chain[option_chain['CPType'] == 'CE']
+    pe_data = option_chain[option_chain['CPType'] == 'PE']
+    i = np.array(pe_data['StrikeRate'])[0]
+    end = np.array(pe_data['StrikeRate'])[-1]
+    ss = np.array(pe_data['StrikeRate'])
+    p_lastrate = np.array(list(pe_data['LastRate']))
+    c_lastrate = np.array(list(ce_data['LastRate']))
+    p_openinterest = np.array(list(pe_data['OpenInterest']))
+    c_openinterest = np.array(list(ce_data['OpenInterest']))
+
+    def loss_function(v):
+        init_pe = np.dot(p_lastrate, p_openinterest)
+        init_ce = np.dot(c_lastrate, c_openinterest)
+        tmax = ss-v[0]
+        tmax[tmax < 0] = 0
+        tmin = v[0]-ss
+        tmin[tmin < 0] = 0
+        end_pe = np.dot(p_openinterest, tmax)
+        end_ce = np.dot(c_openinterest, tmin)
+        data = init_ce-end_ce-init_pe+end_pe
+        return abs(data)
+    a, b = pso(func=loss_function, lb=[i], ub=[end], minfunc=0.1)
+    return np.round_(a[0], 1)
 
 
 def data(week):
@@ -204,119 +201,78 @@ def data(week):
     return option_chain, x
 
 
-def is_expiry(week0):
-    exchange = 'BANKNIFTY'
-    expiry_timestamps = prime_client['login'].get_expiry(
-        "N", exchange).copy()
-    current_time = time.time()
-    week0time_stamp = int(
-        expiry_timestamps['Expiry'][week0]['ExpiryDate'][6:16])
-    if week0time_stamp-current_time < 55800:
-        return 1
-    else:
-        return 0
-
-
-def initial_trades(week0, max_lots, decider):
-    option_chain_week0, x = data(week0)
-    exclusive_strike = int(np.round(x/100)*100)
-    pe_data = option_chain_week0[option_chain_week0['CPType'] == 'PE']
-    ce_data = option_chain_week0[option_chain_week0['CPType'] == 'CE']
-    p_sumB = float(pe_data[pe_data['StrikeRate'] ==
-                   exclusive_strike]['LastRate'])
-    c_sumB = float(ce_data[ce_data['StrikeRate'] ==
-                   exclusive_strike]['LastRate'])
-    net_sum = p_sumB+c_sumB
-    final_strike = exclusive_strike + \
-        (decider > 0.5)*int(np.round(net_sum/100)*100)
-    order_button(final_strike, 'PE_B', max_lots, week0)
-    order_button(final_strike, 'CE_B', max_lots, week0)
-    max_move = abs(exclusive_strike-final_strike)
-    return final_strike, max_move
-
-
-def strategy(orders_tracker, x):
-    init_x = orders_tracker['init_x']
-    final_strike = orders_tracker['final_strike']
-    max_lots = orders_tracker['max_lots']
-    week0 = orders_tracker['week0']
-    max_move = orders_tracker['max_move']
-    losses = orders_tracker['losses']
-    if abs(init_x-x) > max_move:
-        order_button(final_strike, 'PE_S', max_lots, week0)
-        order_button(final_strike, 'CE_S', max_lots, week0)
-        if final_strike != int(np.round(x/100)*100):
-            orders_tracker = {'final_strike': 0,
-                              'max_lots': max_lots,  'week0': week0, 'init_x': x, 'max_move': 0, 'losses': 0}
-        else:
-            orders_tracker = {'final_strike': 0,
-                              'max_lots': max_lots,  'week0': week0, 'init_x': x, 'max_move': 0, 'losses': losses+1}
-    return orders_tracker
-
-
-def premium_decay(premium_sum):
-    option_chain_week0, x = data(week0)
-    exclusive_strike = int(np.round(x/100)*100)
-    pe_data = option_chain_week0[option_chain_week0['CPType'] == 'PE']
-    ce_data = option_chain_week0[option_chain_week0['CPType'] == 'CE']
-    p_sumB = float(pe_data[pe_data['StrikeRate'] ==
-                   exclusive_strike]['LastRate'])
-    c_sumB = float(ce_data[ce_data['StrikeRate'] ==
-                   exclusive_strike]['LastRate'])
-    net_sum = p_sumB+c_sumB
-    return premium_sum-net_sum
-
-
 # %%
-client_name = input('enter the client name: ')
+client_name = "bhaskar"
 prime_client = client_login(client=client_name)
-
-max_lots = int(input('max lots : '))
-week0 = int(input('enter the buying week'))
-ind_time = datetime.now(timezone("Asia/Kolkata")
-                        ).strftime('%Y-%m-%d %H:%M:%S.%f')
-while int(ind_time[11:13])*60+int(ind_time[14:16]) < 600 or int(ind_time[11:13])*60+int(ind_time[14:16]) > 1085:
-    ind_time = datetime.now(timezone("Asia/Kolkata")
-                            ).strftime('%Y-%m-%d %H:%M:%S.%f')
 # %%
-option_chain, x = data(week0)
-decider = np.random.rand()
 
-# option_chain_week0, x = data(week0)
-# exclusive_strike = int(np.round(x/100)*100)
-# pe_data = option_chain_week0[option_chain_week0['CPType'] == 'PE']
-# ce_data = option_chain_week0[option_chain_week0['CPType'] == 'CE']
-# p_sumB = float(pe_data[pe_data['StrikeRate'] ==
-#               exclusive_strike]['LastRate'])
-# c_sumB = float(ce_data[ce_data['StrikeRate'] ==
-#               exclusive_strike]['LastRate'])
-# premium_sum = p_sumB+c_sumB
+d0 = data(0)
+
+# %%
+
+# initial trades
+
+
+def initial_trades(option_chain, x, tron_s, week):
+    exclusive_strike = int(np.round((x)/100)*100)
+    f = np.sum(option_chain[option_chain['StrikeRate']
+               == int(np.round(x/100)*100)]['LastRate'])
+    c_strike_s = exclusive_strike+int(np.ceil(f/200)*100)
+    c_strike_b = exclusive_strike+int(np.ceil(f/100)*100)
+    ce_data = option_chain[option_chain['CPType'] == 'CE']
+    price_b = ce_data[ce_data['StrikeRate'] == int(
+        np.round(c_strike_b/100)*100)]['LastRate'].iloc[0]
+    price_s = ce_data[ce_data['StrikeRate'] == int(
+        np.round(c_strike_s/100)*100)]['LastRate'].iloc[0]
+    tron_b = int(tron_s*(price_s/price_b))
+    order_button(c_strike_b, 'CE_B', tron_b, week)
+    order_button(c_strike_s, 'CE_S', tron_s, week)
+    return tron_s, tron_b, c_strike_s, c_strike_b
+
+# %%
+
+
+def strategy(d, tron_s, tron_b, c_strike_s, c_strike_b, week):
+    option_chain, x = d[0], d[1]
+    exclusive_strike = int(np.round((x)/100)*100)
+    f = np.sum(option_chain[option_chain['StrikeRate']
+               == int(np.round(x/100)*100)]['LastRate'])
+    c_strike_s_new = exclusive_strike+int(np.ceil(f/200)*100)
+    c_strike_b_new = exclusive_strike+int(np.ceil(f/100)*100)
+    if c_strike_b_new != c_strike_b and c_strike_s_new != c_strike_s and x > c_strike_b:
+        order_button(c_strike_s, 'CE_B', tron_s, week)
+        order_button(c_strike_b, 'CE_S', tron_b, week)
+        ce_data = option_chain[option_chain['CPType'] == 'CE']
+        cs_ltp = ce_data[ce_data['StrikeRate'] == int(
+            np.round(c_strike_s/100)*100)]['LastRate'].iloc[0]
+        cb_ltp = ce_data[ce_data['StrikeRate'] == int(
+            np.round(c_strike_b/100)*100)]['LastRate'].iloc[0]
+        tron_b = int(tron_s*(cs_ltp/cb_ltp))
+        order_button(c_strike_b_new, 'CE_B', tron_b, week)
+        order_button(c_strike_s_new, 'CE_S', tron_s, week)
+        c_strike_s = c_strike_s_new
+        c_strike_b = c_strike_b_new
+    if c_strike_b_new != c_strike_b and c_strike_s_new == c_strike_s:
+        tron_b1 = tron_b
+        ce_data = option_chain[option_chain['CPType'] == 'CE']
+        cs_ltp = ce_data[ce_data['StrikeRate'] == int(
+            np.round(c_strike_s/100)*100)]['LastRate'].iloc[0]
+        cb_ltp = ce_data[ce_data['StrikeRate'] == int(
+            np.round(c_strike_b/100)*100)]['LastRate'].iloc[0]
+        tron_b = int(tron_s*(cs_ltp/cb_ltp))
+        order_button(c_strike_b_new, 'CE_B', tron_b, week)
+        order_button(c_strike_b, 'CE_S', tron_b1, week)
+        c_strike_s = c_strike_s_new
+        c_strike_b = c_strike_b_new
+    return tron_s, tron_b, c_strike_s, c_strike_b
+
+
+week = 0
+tron_s = 3
+d = data(week)
+tron_s, tron_b, c_strike_s, c_strike_b = initial_trades(
+    d[0], d[1], tron_s, week)
 while True:
-    option_chain_week0, x = data(week0)
-    new_strike = int(np.round(x/100)*100)
-    # if premium_decay(premium_sum) > 80 and new_strike == exclusive_strike:
-    #    break
-    break
-final_strike, max_move = initial_trades(week0, max_lots, decider)
-orders_tracker = {'final_strike': final_strike,
-                  'max_lots': max_lots,  'week0': week0, 'init_x': x, 'max_move': max_move, 'losses': 0}
-
-# %%
-while int(ind_time[11:13])*60+int(ind_time[14:16]) < 899:
-    ind_time = datetime.now(timezone("Asia/Kolkata")
-                            ).strftime('%Y-%m-%d %H:%M:%S.%f')
-    option_chain, x = data(week0)
-    # x = int(input('x= '))
-    orders_tracker = strategy(orders_tracker, x)
-    if orders_tracker['final_strike'] == 0:
-        decider = np.random.rand()
-        losses = orders_tracker['losses']
-        max_lots += losses
-        final_strike, max_move = initial_trades(week0, max_lots, decider)
-        orders_tracker['final_strike'] = final_strike
-        orders_tracker['max_move'] = max_move
-        orders_tracker['max_lots'] = max_lots
-
-orders_tracker = strategy(orders_tracker, 0)
-
-# %%
+    d = data(0)
+    tron_s, tron_b, c_strike_s, c_strike_b = strategy(
+        d, tron_s, tron_b, c_strike_s, c_strike_b, week)
